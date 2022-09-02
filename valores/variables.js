6ed858c0dedcc6ee7f2c7b942791dff0011f8c08
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2022-09-02 00:58:21 -0500

    uso de variables en javascrip´´t

---
+++ valores/variables.js
@@ -0,0 +1,12 @@
+var nombre = "oscar"; //Varible tipo string
+var edad = 10; //Variable numerica
+var boolena = false; //Variable boolena
+
+/*Variable tipo arreglo*/
+var elementos = ["Computadora","Celular"]; 
+
+/*Variable tipo objeto*/
+var persona = {
+    nombre: "Miguel",
+    edad: 30
+}
