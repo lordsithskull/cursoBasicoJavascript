7393cfda3a90fb172d2c794f5eee44f246e8fa57
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2022-09-08 23:13:25 -0500

    uso de funciones

---
+++ funciones/funciones.js
@@ -0,0 +1,17 @@
+//Declarativas
+ function miFuncion1() {
+    return 3;
+ }
+
+ //Expresion 
+ var miFuncion = function (a, b) {
+    return a + b; 
+ }
+
+ miFuncion(5,3); //llamada
+
+
+ function suma (a,b) {
+    var resultado = a + b;
+    return resultado;
+}
