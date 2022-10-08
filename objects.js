6d554ee429015a912c905093490467f6a56f6a63
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2022-10-08 16:09:47 -0500

    objetos y contructores

--- objects.js
+++ objects.js
@@ -7,4 +7,17 @@ var miAuto = {
     detalleDelAuto: function () {
         console.log(`Auto ${this.modelo} ${this.annio}`);
     }
+} 
+
+//Funcion constructor 
+function auto (marca, modelo, annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
 }
+
+//NUEVAS INSTANCIAS DE AUTO 
+var autoNuevo = new auto("volkswagen", "Vocho",1980) ;
+var autoNuevo2 = new auto("Tesla", "Model X",2018) ;
+var autoNuevo3 = new auto("Toyota", "Corolla",2020) ;
+
