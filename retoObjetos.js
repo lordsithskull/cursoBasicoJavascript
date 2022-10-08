dfd46ae86ece2516365fa4a4ae70decc66825f0e
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2022-10-08 16:28:10 -0500

    reto auto

---
+++ retoObjetos.js
@@ -0,0 +1,21 @@
+function auto (MARCA, MODELO, ANNIO) {
+    this.marca = MARCA;
+    this.modelo = MODELO;
+    this.annio = ANNIO;
+}
++
+var autos = [];
+
+//La instrucción let declara una variable de alcance local con ámbito de bloque(blockscope)
+for (let i = 0; i < 3; i++){
+    // prompt muestra un diálogo con mensaje opcional, que solicita al usuario que introduzca un texto.
+    var marca = prompt("Ingresar marca del auto");
+    var modelo = prompt("Ingresar modelo del auto");
+    var annio = prompt("Ingresar año del auto");
+
+    autos.push(new auto (marca, modelo, annio))
+}
+
+for (let i = 0; i < autos.length; i++){
+    console.log(autos[i]);
+}
